8eee908073e30d2b517109da6f7e424464b5df5e
biefan <70761325+biefan@users.noreply.github.com>
2021-07-02 15:52:12 +0800

    Add files via upload

---
+++ dome.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class dome {
+    public static void main(String[] args) {
+
+        Scanner bf = new Scanner(System.in);
+            int adg = bf.nextInt();
+        System.out.println("请输入我爱你：");
+        System.out.println(adg);
+
+    }
+}
